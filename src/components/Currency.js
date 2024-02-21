87ad727c3d8f2cbbca4c3d2cad53d79fa1253b83
sherrera90 <steph.herrera90@gmail.com>
2024-02-21 11:22:58 -0500

    dispatch currency

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,31 +1,36 @@
 
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { AppContext } from '../context/AppContext';
 
 
-    // const [newCurrency, setNewCurrency] = useState(currency);
+const Currency =()=>{
+    const { dispatch } = useContext(AppContext);
+    const [newCurrency, setNewCurrency] = useState('DropDown');
+
+    const clickEvent = (event) => {
+        setNewCurrency(event);
     
-    // const clickEvent = (event) => {
-    //     setNewCurrency(event.target.value);
-    // }
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event,
+        });
+    }
 
-
-function Currency() {
     return (
       <Dropdown>
         <Dropdown.Toggle style={{backgroundColor:'#D1E7DD'}} id="dropdown-basic">
-          Dropdown Button
+        {newCurrency}
         </Dropdown.Toggle>
   
         <Dropdown.Menu style={{backgroundColor:'#D1E7DD'}}>
-          {/* <Dropdown.Item value="newCurrency" onClick={clickEvent} >$ Dollars</Dropdown.Item> */}
-          <Dropdown.Item href="#/action-2">£ Pound</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">€ Euro</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">₹ Rupee</Dropdown.Item>
+        <Dropdown.Item onClick={() => clickEvent('$')}> $ Dollars</Dropdown.Item> 
+          <Dropdown.Item onClick={() => clickEvent('£')}>£ Pound</Dropdown.Item>
+          <Dropdown.Item onClick={() => clickEvent('€')}>€ Euro</Dropdown.Item>
+          <Dropdown.Item onClick={() => clickEvent('₹')}>₹ Rupee</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     );
   };
   
-
   export default Currency;
